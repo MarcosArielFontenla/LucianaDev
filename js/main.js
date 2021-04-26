7553db5befe1ffd7d34274a7937b34179cd8af2e
Marcos Ariel Fontenla <marcos.fontenla@hotmail.com>
2021-04-26 15:02:35 -0300

    version 1.0

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Menu mostrar y colapsar
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close')
+
+// Mostrar
+toggleMenu.addEventListener('click', ()=> {
+    navMenu.classList.toggle('show');
+})
+
+// Colapsar
+closeMenu.addEventListener('click', ()=> {
+    navMenu.classList.remove('show');
+})
+
+// Remover menu
+const navLink = document.querySelectorAll('.nav-link');
+
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Seleccion con scroll el link active
+const sections = document.querySelectorAll('section[id]');
+window.addEventListener('scroll', scrollActive);
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href *= '+ sectionId +']').classList.add('active');
+        } else {
+            document.querySelector('.nav-menu a[href *= '+ sectionId +']').classList.remove('active');
+        }
+    })
+}
